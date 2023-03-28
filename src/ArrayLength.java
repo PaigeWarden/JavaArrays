fab1bdd67a3a5310ddc4b630e340f34a0015e695
Paige <paigewarden123@gmail.com>
2023-03-28 21:24:27 +0100

    Array basics

---
+++ src/ArrayLength.java
@@ -0,0 +1,8 @@
+public class ArrayLength {
+    public static void main(String[] args) {
+
+            String[] cars = {"Volvo", "BWD", "Ford", "Mazda"};
+            System.out.println(cars.length);
+        }
+    }
+
